9c520f957cf3c0a663550cab79db065b61c688f5
nmpereira <nikhilpereira25@gmail.com>
2022-02-15 14:14:20 -0500

    inital ws

--- app/server/index.js
+++ app/server/index.js
@@ -5,15 +5,32 @@ const path = require("path");
 const logger = require("./routes/logger");
 const api = require("./routes/api");
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 7071 });
 const mongoose = require("mongoose");
+
+wss.on("connection", function connection(ws) {
+  console.log("A new client Connected!");
+  ws.send("Welcome New Client!");
+
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
 
 mongoose.connect(process.env.dbURI);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.error("Connected to db"));
 
-const app = express();
 app.set("json spaces", 2);
 app.use(express.json());
 // Have Node serve the files for our built React app
