abd227813e4583d3f4f94c46702f8136ef8755c2
nmpereira <nikhilpereira25@gmail.com>
2022-02-15 20:38:52 -0500

    Update socketClient.js

--- app/server/public/socketClient.js
+++ app/server/public/socketClient.js
@@ -1,5 +1,24 @@
 // Create WebSocket connection.
-const ws = new WebSocket("wss://time-share.herokuapp.com:7071");
+
+var loc = window.location,
+  new_uri;
+if (loc.protocol === "https:") {
+  new_uri = "wss:";
+} else {
+  new_uri = "ws:";
+}
+// console.log("1 ", new_uri);
+new_uri += "//" + loc.host.replace(loc.port, "7071");
+// console.log("2 ", new_uri);
+// new_uri += loc.origin.replace(loc.port, "7071");
+// console.log("3 ", new_uri);
+// console.log(loc);
+// console.log(loc.hostname);
+// console.log(loc.port);
+
+var HOST = new_uri;
+// console.log("HOST: ", HOST);
+var ws = new WebSocket(HOST);
 
 // Connection opened
 ws.addEventListener("open", function (event) {
