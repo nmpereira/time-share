ae4c339ae298efc7521ea46efec5c2148b025d46
nmpereira <nikhilpereira25@gmail.com>
2022-03-17 16:59:46 -0400

    implement redirect

--- app/server/routes/times.js
+++ app/server/routes/times.js
@@ -73,7 +73,7 @@ router.route("/").post(async (req, res, next) => {
 
   try {
     const newTime = await Time.save();
-    res.status(201).json(newTime);
+    res.redirect(`/api/times/time/${newTime._id}`);
   } catch (err) {
     res.status(400).json({ msg: err.message });
   }
