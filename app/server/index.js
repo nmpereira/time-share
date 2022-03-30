cedbbffa75295d1aa1c94acda9949a87704f779f
Nikhil Mario Pereira <45009203+nmpereira@users.noreply.github.com>
2022-03-30 01:47:45 -0400

    Add timer update feature (#9)
    * remove unused server logs
    
    * added error page for non existent timer route
    
    * added footer + removed deprecated joinroom code
    
    * fixed localuser connection counts
    
    * changed position of local user counter to footer
    
    * add conditional to local timer to show only on timer page
    
    * add conditionals for pause/resume buttons
    
    * change button colors
    
    * Update snackbar.css
    
    * added description to homepage
    
    * add reset timer route
    
    * add reset timer redirect
    
    * Reset timer (#8)
    
    * set up route to reset timer
    
    * fixed the reset timer
    
    will clean code later :)
    
    * update reset route to be more semantic+ removed console logs

--- app/server/index.js
+++ app/server/index.js
@@ -15,12 +15,14 @@ let timestamp = moment().add(10, "seconds");
 const helpers = require("./routes/helpers");
 // const runATimer = require("runTheTimer");
 // const joinARoom = require("joinRoom");
-const joinARoom = require("./index.js");
+// const joinARoom = require("./index.js");
 const runATimer = require("./index.js");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = new Server(server);
+const methodOverride = require("method-override");
+const time = require("../server/models/time");
 
 app
   .use(express.static(path.resolve(__dirname, "../server/public")))
@@ -28,6 +30,7 @@ app
   .use(logger)
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
+  .use(methodOverride("_method"))
   .set("json spaces", 2)
   .set("view engine", "ejs")
   .set("views", path.join(__dirname, "views"))
@@ -40,48 +43,145 @@ app
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 //Get single Time by id
 app.get("/:id", async (req, res) => {
+  let userID = req.params.id;
+  let reqHost = req.headers.host;
+  helpers.endTime(reqHost, userID).then((e) => {
+    if (!e) {
+      res.render("../public/error");
+    } else {
+      try {
+        res.render("../public/timeshare");
+      } catch (err) {
+        res.status(500).json({ msg: err.message });
+      }
+
+      setTimeout(() => {
+        // joinARoom.joinRoom(userID);
+        runATimer.runTheTimer(
+          helpers.endTime(reqHost, userID).then((e) => {
+            return e;
+          }),
+          userID
+        );
+      }, 500);
+    }
+  });
+});
+app.get("/reset/:id", async (req, res) => {
+  var id = req.params.id;
   try {
-    res.render("../public/timeshare");
+    res.render("../public/resettimer", { userid: id });
   } catch (err) {
     res.status(500).json({ msg: err.message });
   }
-
-  let userID = req.params.id;
-  let reqHost = req.headers.host;
-
-  setTimeout(() => {
-    joinARoom.joinRoom(userID);
-    runATimer.runTheTimer(
-      helpers.endTime(reqHost, userID).then((e) => {
-        return e;
-      }),
-      userID
-    );
-  }, 500);
 });
+
+//Update single Time by id
+app.post("/reset/:id", async (req, res) => {
+  // console.log("abc");
+  const userID = req.params.id;
+  let reqHost = req.headers.host;
+  const query = { user: req.params.id };
+  const updated_at = Date.now();
+  const update = {
+    $set: {
+      user: req.body.user,
+      num_work: req.body.num_work,
+      time_work: req.body.time_work,
+      num_break: req.body.num_break,
+      time_break: req.body.time_break,
+      sets: req.body.sets,
+      end_time: req.body.end_time,
+      paused: req.body.paused,
+    },
+    updated_at,
+  };
+
+  try {
+    const times = await time.findOneAndUpdate(query, update, {
+      new: true,
+    });
+
+    // res.send(times);
+    res.redirect(`/${userID}`);
+
+    clearInterval(runningTimerTrak[userID].interval);
+    runningTimerTrak[userID].interval = null;
+    setTimeout(() => {
+      // joinARoom.joinRoom(userID);
+      runATimer.runTheTimer(
+        helpers.endTime(reqHost, userID).then((e) => {
+          return e;
+        }),
+        userID
+      );
+    }, 500);
+  } catch (err) {
+    res.status(500).json({ msg1: err.message });
+    console.log(err);
+  }
+});
+// app.put("/reset/:id", async (req, res) => {
+//   console.log("def");
+//   // var userid = req.body.userid;
+//   // var endtime = req.body.endtime;
+//   console.log("inputs:");
+//   // const query = { user: req.params.id };
+//   // const updated_at = Date.now();
+//   // const update = {
+//   //   $set: {
+//   //     user: req.body.user,
+//   //     num_work: req.body.num_work,
+//   //     time_work: req.body.time_work,
+//   //     num_break: req.body.num_break,
+//   //     time_break: req.body.time_break,
+//   //     sets: req.body.sets,
+//   //     end_time: req.body.end_time,
+//   //     paused: req.body.paused,
+//   //   },
+//   //   updated_at,
+//   // };
+
+//   // try {
+//   //   const times = await time.findOneAndUpdate(query, update, {
+//   //     new: true,
+//   //   });
+//   //   res.send(times);
+//   // } catch (err) {
+//   //   res.status(500).json({ msg1: err.message });
+//   // }
+// });
+
 //Websocket
 let clientsConnected_Global = 0;
 let run;
 io.on("connection", (socket) => {
-  console.log("New client Connected!");
+  const roomID = socket.handshake.headers.referer.split("/").pop();
+  console.log("New client Connected!", "Room ID:", roomID);
+
+  socket.join(roomID);
 
   clientsConnected_Global += 1;
   io.emit("userActivity", {
     clientsConnected_Global,
     Activity: "Client Joined",
   });
-  console.log("Global Connections", clientsConnected_Global);
+  // console.log("Global Connections", clientsConnected_Global);
 
   //Whenever someone disconnects this piece of code executed
-  socket.on("disconnect", function () {
+  socket.on("disconnect", function (reason) {
     clientsConnected_Global -= 1;
     io.emit("userActivity", {
       clientsConnected_Global,
       Activity: "Client Left",
     });
-    console.log("Global Connections", clientsConnected_Global);
+    // console.log("Global Connections", clientsConnected_Global);
 
-    console.log("Client has Disconnected");
+    console.log(
+      reason
+        ? `Client has Disconnected from:  ${roomID} due to ${reason}`
+        : "Client has Disconnected"
+    );
   });
 
   //send a timestamp to the socket
@@ -97,7 +197,7 @@ io.on("connection", (socket) => {
         (runningTimerTrak[msg.userId].running = false),
       ]);
     }
-    console.log("pausetimer", msg);
+    // console.log("pausetimer", msg);
   });
   socket.on("playtimer", function (msg) {
     const currentRoom = runningTimerTrak[msg.userId];
@@ -107,10 +207,12 @@ io.on("connection", (socket) => {
         (runningTimerTrak[msg.userId].running = true),
       ]);
     }
-    console.log("playtimer");
+    // console.log("playtimer");
   });
   socket.on("resettimer", function (msg) {
-    console.log("resettimer");
+    // let reset;
+    // runningTimerTrak[reset] = true;
+    // console.log("resettimer", runningTimerTrak[reset]);
   });
 
   function sendAMessage(msg) {
@@ -121,22 +223,26 @@ io.on("connection", (socket) => {
     msg = msg.then((msg) => {
       return timeFromNow(msg);
     });
-    console.log(`timestamp: request from user:${userID}`);
+    // console.log(`timestamp: request from user:${userID}`);
     // run = true;
     runTimer(socket, msg, msg);
   }
-  function joinRoom(userID) {
-    socket.join(userID);
-    console.log("Joined room:" + userID);
-    socket.emit("message", `Joined a room: ${userID}`);
-  }
+  // function joinRoom(userID) {
+  //   socket.join(userID);
+  //   console.log("Joined room:" + userID);
+  //   socket.emit("message", `Joined a room: ${userID}`);
+  // }
 
   module.exports.sendAMessage = sendAMessage;
   module.exports.runTheTimer = runTheTimer;
-  module.exports.joinRoom = joinRoom;
+  // module.exports.joinRoom = joinRoom;
 });
 const runningTimerTrak = {};
 const runTimer = async (socket, input, msg) => {
+  // var ref;
+  // clearInterval(ref);
+  let reset;
+  // console.log("you are in runTimer");
   const clientsConnected_Socket = 0;
   socket.emit("message", "Greetings Earthling");
   const roomID = socket.handshake.headers.referer.split("/").pop();
@@ -147,48 +253,76 @@ const runTimer = async (socket, input, msg) => {
     const currentRoom = runningTimerTrak[roomID];
 
     if (!currentRoom) return;
-    currentRoom.clients.splice(currentRoom.clients.indexOf(socket), 1);
+    // NOTE: find index of room and return if not found
+    const socket_index = currentRoom.clients.indexOf(socket);
+    if (socket_index == -1) return;
+    // console.log("socket_index_1", socket_index);
+    currentRoom.clients.splice(socket_index, 1);
+
     runningTimerTrak[roomID].connections -= 1;
-    io.emit("localUserActivity", {
+    io.to(roomID).emit("localUserActivity", {
       clientsConnected_Socket: runningTimerTrak[roomID].connections,
       Activity: "Socket Client Left",
+      roomID,
     });
-    console.log("Local Connections", runningTimerTrak[roomID].connections);
+    // console.log("Local Connections", runningTimerTrak[roomID].connections);
   });
   // console.log("runningTimerTrak1", runningTimerTrak);
   // if (runningTimerTrak[roomID]) return;
   const param = await input;
-  socket.emit("timestamp", formatter("run", secondsToHMS(param), param <= 0));
-  if (runningTimerTrak[roomID] !== undefined) {
+  socket.emit("timestamp", formatter("Loading", "Loading...", param <= 0));
+  // NOTE: if exists or if interval is running
+  if (
+    runningTimerTrak[roomID] !== undefined &&
+    runningTimerTrak[roomID].interval != null
+  ) {
     runningTimerTrak[roomID].clients.push(socket);
     runningTimerTrak[roomID].connections += 1;
-    io.emit("localUserActivity", {
+    io.to(roomID).emit("localUserActivity", {
       clientsConnected_Socket: runningTimerTrak[roomID].connections,
       Activity: "Socket Client Joined",
+      roomID,
     });
-    console.log("Local Connections", runningTimerTrak[roomID].connections);
+
+    // console.log(
+    //   "Local Connections",
+    //   runningTimerTrak[roomID].connections,
+    //   roomID
+    // );
+    // console.log("you are in if Notundefined");
+    // NOTE: i am here. I need to add a property to an object "reset:true" and make sure it doesnt return here but goes and runs timer
     return;
   }
-  runningTimerTrak[roomID] = {
-    running: true,
-    clients: [socket],
-    connections: 1,
-  };
-  io.emit("localUserActivity", {
+  // console.log("runningTimerTrak[roomID]_1", runningTimerTrak[roomID]);
+  if (runningTimerTrak[roomID] == undefined) {
+    runningTimerTrak[roomID] = {
+      running: true,
+      clients: [socket],
+      connections: 1,
+      interval: null,
+    };
+  }
+  // console.log("runningTimerTrak[roomID]_2", runningTimerTrak[roomID]);
+  io.to(roomID).emit("localUserActivity", {
     clientsConnected_Socket: runningTimerTrak[roomID].connections,
     Activity: "User Joined new room",
+    roomID,
   });
-  console.log("Local Connections", runningTimerTrak[roomID].connections);
+  // console.log("Local Connections", runningTimerTrak[roomID].connections);
 
   // console.log("runningTimerTrak2", runningTimerTrak);
-  var ref;
+  // var ref;
 
+  // add the interval (ref) to the room obj (runningTimerTrak[roomID])
+  //
+
+  //
   function longForLoop(param) {
-    console.log("timeleft", param);
+    // console.log("timeleft", param);
     var i = param;
 
     if (param > 0) {
-      ref = setInterval(() => {
+      runningTimerTrak[roomID].interval = setInterval(() => {
         // console.log("i and rooomID", i, roomID);
         if (runningTimerTrak[roomID].running === true) {
           i--;
@@ -200,7 +334,7 @@ const runTimer = async (socket, input, msg) => {
             s.emit("timestamp", formatter("paused", secondsToHMS(i), false));
           });
         }
-        if (i <= 0) clearInterval(ref);
+        if (i <= 0) clearInterval(runningTimerTrak[roomID].interval);
       }, 1000);
     }
   }
--- app/server/routes/helpers.js
+++ app/server/routes/helpers.js
@@ -2,11 +2,12 @@ const fetch = require("node-fetch");
 
 // Handle GET requests to /api route
 const endTime = async (reqHost, userID) => {
-  console.log(`Host: ${reqHost}`);
+  // console.log(`Host: ${reqHost}`);
   let query = `http://${reqHost}/api/times/${userID}`;
   const fetchtest = await fetch(query);
   const fetchData = await fetchtest.json();
   let timestamp = fetchData.end_time;
+
   return timestamp;
 };
 module.exports = { endTime };
