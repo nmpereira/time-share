b3fd0a03b3a7d857e88718bc2141e3c1ed85f0c0
nmpereira <nikhilpereira25@gmail.com>
2022-04-18 21:16:53 -0400

    bugfix: fix error when user is not in db

--- app/server/index.js
+++ app/server/index.js
@@ -285,8 +285,8 @@ const writePomoToDb = async (_roomID, pomo_count, break_count) => {
 
 const readUpdateLogToDb = async (_roomID) => {
   const data = await readFromDb(_roomID);
-  const updateLog = data.updateLog;
-  // console.log("updateLog2", updateLog);
+  const updateLog = data?.updateLog || [];
+  console.log("updateLog2", updateLog);
   return updateLog;
 };
 const writeUpdateLogToDb = async (_roomID, event) => {
--- app/server/public/socketClient.js
+++ app/server/public/socketClient.js
@@ -20,23 +20,23 @@
 // });
 
 // Listen for messages
-socket.addEventListener("message", function (event) {
-  console.log("Message from server ", event.data);
-});
-socket.addEventListener("timestamp", function (event) {
-  console.log(event);
-  console.log("123");
-});
-socket.on("message", (msg) => {
-  console.log(msg);
-  console.log(456);
-}); // From server
-socket.on("timestamp", (msg) => {
-  console.log(msg);
-  console.log(123);
-}); // From server
-const user = document.getElementById("user");
-const sendMessage = () => {
-  socket.emit("message", `Hello From: ${user.value}`);
-  console.log(`Hello From: ${user.value}`);
-};
+// socket.addEventListener("message", function (event) {
+//   console.log("Message from server ", event.data);
+// });
+// socket.addEventListener("timestamp", function (event) {
+//   console.log(event);
+//   console.log("123");
+// });
+// socket.on("message", (msg) => {
+//   console.log(msg);
+//   console.log(456);
+// }); // From server
+// socket.on("timestamp", (msg) => {
+//   console.log(msg);
+//   console.log(123);
+// }); // From server
+// const user = document.getElementById("user");
+// const sendMessage = () => {
+//   socket.emit("message", `Hello From: ${user.value}`);
+//   console.log(`Hello From: ${user.value}`);
+// };
